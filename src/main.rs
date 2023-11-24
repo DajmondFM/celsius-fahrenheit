1a0bfe32453930ba8d15ff6ea5a3b43730194ac5
DajmondFM <franekmalikowski@gmail.com>
2023-11-24 22:24:47 +0100

    tak

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
 use std::io;
-fn c_to_f() {
-    let c = input();
-    let mut f:f32= 32.0;
-    f = f+c*1.8;
-    println!("{}", f.round());
-}
+use std::env;
+
 // 1c == 1.8f
 
-fn f_to_c() {
-    let f = input();
-    let c:f32= (f-32.0)/1.8;
-    println!("{}", c.round());
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        1 => println!("No argument given. Try cf to convert celsius to fahrenheit and fc to convert fahrenheit to celsius."),
+        2 =>    match args[1].as_str() {
+                "cf" => c_to_f(),
+                "fc" => f_to_c(),
+                _ => println!("Type cf to convert celsius to fahrenheit and fc to convert fahrenheit to celsius."),
+                }
+        _ => println!("Too many arguments."),
+    }
 }
+
 
 fn input() -> f32 {
     let mut number = String::new();
@@ -23,17 +27,16 @@ fn input() -> f32 {
 }
 
 
-use std::env;
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    match args.len() {
-        1 => println!("No argument given. Try cf to convert celsius to fahrenheit and fc to convert fahrenheit to celsius."),
-        2 =>    match args[1].as_str() {
-                "cf" => c_to_f(),
-                "fc" => f_to_c(),
-                _ => println!("Type cf to convert celsius to fahrenheit and fc to convert fahrenheit to celsius."),
-                }
-        _ => println!("Too many arguments."),
-        
-    }
+fn f_to_c() {
+    let f = input();
+    let c:f32= (f-32.0)/1.8;
+    println!("{}", c.round());
+}
+
+
+fn c_to_f() {
+    let c = input();
+    let mut f:f32= 32.0;
+    f = f+c*1.8;
+    println!("{}", f.round());
 }
